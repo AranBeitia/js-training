07e64ce10f922388fdb266f0f13914399ad25cc1
Arantza Beitia <abeitia@theinit.com>
2019-09-23 17:36:26 +0200

    Training

--- fundamentos/15.switch.js
+++ fundamentos/15.switch.js
@@ -1,62 +1,59 @@
-//(video 36)
-// // Switch
+// Switch
+const metodoPago = "bnmgh";
 
-// const metodoPago = "bitcoins";
+switch (metodoPago){
+	case "efectivo":
+		console.log(`Ha pagado con ${metodoPago}`);
+		break;
+	case "tarjeta":
+		console.log(`Ha pagado con ${metodoPago}`);
+		break;
+	case "bitcoins":
+		console.log(`Ha pagado con ${metodoPago}`);
+		break;
+	default:
+		console.log(`Metodo de pago no soportado`);
+		break;
+}
 
-// switch (metodoPago) {
-//   case "efectivo":
-//     console.log(`Ha pagado con ${metodoPago}`);
-//     break;
-//   case "cheque":
-//     console.log(`Ha pagado con ${metodoPago}`);
-//     break;
-//   case "tarjeta":
-//     console.log(`Ha pagado con ${metodoPago}`);
-//     break;
-//   default:
-//     console.log("Método de pago no soportado");
-//     break;
-// }
+let mes;
 
-// let mes;
-
-// switch (new Date().getMonth()) {
-//   case 0:
-//     mes = "Enero";
-//     break;
-//   case 1:
-//     mes = "Febrero";
-//     break;
-//   case 2:
-//     mes = "Marzo";
-//     break;
-//   case 3:
-//     mes = "Abril";
-//     break;
-//   case 4:
-//     mes = "Mayo";
-//     break;
-//   case 5:
-//     mes = "Junio";
-//     break;
-//   case 6:
-//     mes = "Julio";
-//     break;
-//   case 7:
-//     mes = "Agosto";
-//     break;
-//   case 8:
-//     mes = "Septiembre";
-//     break;
-//   case 9:
-//     mes = "Octubre";
-//     break;
-//   case 10:
-//     mes = "Noviembre";
-//     break;
-//   case 11:
-//     mes = "Diciembre";
-//     break;
-// }
-
-// console.log(`Estamos en el mes ${mes}`);
+switch (new Date().getUTCMonth()){
+	case 0:
+		mes = "Enero";
+		break;
+	case 1:
+		mes = "Febrero";
+		break;
+	case 2:
+		mes = "Marzo";
+		break;
+	case 3:
+		mes = "Abril";
+		break;
+	case 4:
+		mes = "Mayo";
+		break;
+	case 5:
+		mes = "Junio";
+		break;
+	case 6:
+		mes = "Julio";
+		break;
+	case 7:
+		mes = "Agosto";
+		break;
+	case 8:
+		mes = "Septiembre";
+		break;
+	case 9:
+		mes = "Octubre";
+		break;
+	case 10:
+		mes = "Noviembre";
+		break;
+	case 11:
+		mes = "Diciembre";
+		break;
+}
+console.log(`Estamos en el mes ${mes}`);
