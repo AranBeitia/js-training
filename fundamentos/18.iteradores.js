85f3aa8097c585302dcf942144bcdf462e693836
Arantza Beitia <abeitia@theinit.com>
2019-09-24 09:26:22 +0200

    Training

--- fundamentos/18.iteradores.js
+++ fundamentos/18.iteradores.js
@@ -1,80 +1,76 @@
-// (video 40)
 // Iteradores
-// const ciudades = ['Londres', 'Paris', 'Berlin', 'Madrid'];
-// const ordenes = new Set([123, 231,131,102]);
-// const datos = new Map();
-// datos.set('nombre', 'Juan');
-// datos.set('profesion', 'fronen');
+const ciudades = [`Londres`, `Paris`, `Berlin`, `Mardid`];
+const ordenes = new Set([123, 131, 102]);
+const datos = new Map();
+datos.set(`nombre`, `Juan`);
+datos.set(`profesion`, `desarrollo web`);
 
-// // ENTRIES a las ciudades
-// for (let entrada of ciudades.entries()){
-//   console.log(entrada);
-// }
-// // ENTRIES para las ordenes
-// for (let entradas of ordenes.entries()){
-//   console.log(entradas);
-// }
-// // ENTRIES para el map
-// for (let entrada of datos.entries()){
-//   console.log(entrada);
-// }
+console.log(ciudades);
+console.log(ordenes);
+console.log(datos);
 
-// // VALUES a las ciudades. 
-// //Si ponemos .values() devuelve error, 
-// //así que sin nada será el por defecto que es values
-// for (let entrada of ciudades){
-//   console.log(entrada);
-// }
+// ENTRIES iteradores
 
-// // VALUES para las ordenes
-// for (let entradas of ordenes.values()){
-//   console.log(entradas);
-// }
-// // VALUES para el map
-// for (let entrada of datos.values()){
-//   console.log(entrada);
-// }
+//Entries() a las ciudades
+for (let entrada of ciudades.entries()) {
+	console.log(entrada);
+}
+//Entries() para las ordenes
+for (let entradas of ordenes.entries()) {
+	console.log(entradas);
+}
+//Entries() para el map
+for (let entrada of datos.entries()) {
+	console.log(entrada);
+}
+// VALUES iteradores
 
-// //KEYS iterador
-// for (let entrada of ciudades.keys()){
-//   console.log(entrada);
-// }
+// values() a las ciudades. 
+//Si ponemos .values() devuelve error, 
+//así que sin nada será el por defecto que es values
+for (let entrada of ciudades) {
+	console.log(entrada);
+}
+// values() para el map
+for (let entrada of datos.values()) {
+	console.log(entrada);
+}
+// KEYS iterador
 
-// // KEYS para las ordenes
-// for (let entradas of ordenes.keys()){
-//   console.log(entradas);
-// }
-// // KEYS para el map
-// for (let entrada of datos.keys()){
-//   console.log(entrada);
-// }
+// keys() a ciudades
+for (let entrada of ciudades.keys()) {
+	console.log(entrada);
+}
+// keys() a ordenes
+for (let entrada of ordenes.keys()) {
+	console.log(entrada);
+}
+// keys() a map
+for (let entrada of datos.keys()) {
+	console.log(entrada);
+}
+//DEFAULT iterador
 
-// //DEFAULT iterador
-// for (let entrada of ciudades){
-//   console.log(entrada);
-// }
+for (let entrada of ciudades) {
+	console.log(entrada);
+}
+for (let entrada of ordenes) {
+	console.log(entrada);
+}
+for (let entrada of datos) {
+	console.log(entrada);
+}
 
-// for (let entradas of ordenes){
-//   console.log(entradas);
-// }
+const mensaje = `Aprendiendo JS`;
+for (let i = 0; i < mensaje.length; i++) {
+	console.log(mensaje[i]);
+}
+//forma nueva
+for (let letra of mensaje){
+	console.log(letra);
+}
 
-// for (let entrada of datos){
-//   console.log(entrada);
-// }
-
-// //
-// const mensaje = 'Aprendiento JavaScript';
-// //forma antigüa
-// for (let i = 0; i < mensaje.length; i++){
-//   console.log(mensaje[i]);
-// }
-// //forma nueva
-// for (let letra of mensaje){
-//   console.log(letra);
-// }
-
-// const enlaces = document.getElementsByTagName('a');
-
-// for (let enlace of enlaces){
-//   console.log(enlace.href);
-// }
+const enlaces = document.getElementsByTagName(`a`);
+for (let enlace of enlaces){
+	console.log(enlace.href);
+}
