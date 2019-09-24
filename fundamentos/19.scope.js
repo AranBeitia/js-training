653820fb97b2372005d0ae42547a033d38ad4bf7
AranBeitia <arantza.beitia@gmail.com>
2019-09-24 18:12:44 +0200

    training

--- fundamentos/19.scope.js
+++ fundamentos/19.scope.js
@@ -1,34 +1,35 @@
-// (video 41)
-// var a = 'a';
-// var b = 'b';
-// var c = 'c';
+var a = `a`;
+let b = `b`;
+const c = `c`;
 
-// //Scope de la funcion
-// function funcionScope(){
-//   var a = 'A';
-//   let b = 'B';
-//   const c = 'C';
-//   console.log('FUNCION: ' + a, b, c);
-// }
-// funcionScope();
+//Scope de la funcion
+function funcionScope(){
+	var a = `A`;
+	let b = `B`;
+	const c = `C`;
+	console.log(`FUNCION: ` + a, b, c);
+}
+funcionScope();
 
-// //Scope de bloque()
-// //Let y const sólo están disponibles en su scope, 
-// //no así con var. Por eso no usar var
-// if (true){
-//   var a = 'AA';
-//   let b = 'BB';
-//   const c = 'CC';
-//   console.log('FUNCION: ' + a, b, c);
-// }
+//Scope de bloque{}
+//Let y const sólo están disponibles en su scope, 
+//no así con var. Por eso no usar var
+if (true) {
+	var a = `AA`;
+	let b = `BB`;
+	const c = `CC`;
+	console.log(`BLOQUE: ` + a, b, c);
+}
 
-// //for. Var sobreescribe a
-// for (var a = 0; a<5; a++){
-//   console.log(a);
-// }
-// //let no sobreescribe b
-// for (let b = 0; b<5; b++){
-//   console.log(b);
-// }
+console.log(`GLOBALES: ` + a, b, c);
 
-// console.log('GLOBALES: ' + a,b,c);
+//for. Var sobreescribe a global
+for (var a = 0; a < 5; a++) {
+	console.log(a);
+}
+//let no sobreescribe b
+for (let b = 0; b<5; b++){
+  console.log(b);
+}
+
+console.log('GLOBALES: ' + a,b,c);
