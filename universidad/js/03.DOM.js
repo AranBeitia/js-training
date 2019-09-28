52d0fe0cc516f7d757b3f3c8cfa7667f633235f5
AranBeitia <arantza.beitia@gmail.com>
2019-09-28 12:28:14 +0200

    Training

--- universidad/js/03.DOM.js
+++ universidad/js/03.DOM.js
@@ -10,43 +10,28 @@ console.log(enlaces);
 
 //mezclar querySelector con getElementByClassName
 const listaEnlaces = document.querySelector('#principal').getElementsByClassName('enlace');
-
 console.log(listaEnlaces);
 
+const links = document.getElementsByTagName('a');
+links[18].style.color ='red';
+links[18].textContent = 'Hello kitty satan';
+console.log(links);
 
+let arrayEnlaces = Array.from(links);
+arrayEnlaces.forEach(function(enlace){
+	console.log(enlace.textContent);
+});
 
+//querySelectorAll
+const enlaces2 = document.querySelectorAll('#principal .enlace');
+enlaces2[1].style.background = "#000cff";
+enlaces2[1].style.color = "#fff";
+console.log(enlaces2);
 
+const enlaces3 = document.querySelectorAll('#principal a:nth-child(odd');
+enlaces3.forEach(function(impares){
+	impares.style.background = 'red';
+	impares.style.color = 'white';
+});
 
-
-// const listaEnlaces = document
-//   .querySelector("#principal")
-//   .getElementsByClassName("enlace");
-
-// console.log(listaEnlaces);
-
-// const links = document.getElementsByTagName("a");
-// links[18].style.color = "red";
-// links[18].textContent = "nuevo enlace kitty";
-
-// console.log(links);
-
-// let enlaces2 = Array.from(links);
-
-// enlaces2.forEach(function(enlace) {
-//   console.log(enlace.textContent);
-// });
-// //console.log(enlaces2);
-
-// //querySelectorAll
-// const enlaces3 = document.querySelectorAll("#principal .enlace");
-
-// console.log(enlaces3);
-
-// const enlaces4 = document.querySelectorAll("#principal a:nth-child(odd)");
-
-// enlaces4.forEach(function(impares){
-//   impares.style.backgroundColor = 'red';
-//   impares.style.color = 'white';
-// });
-
-// console.log(enlaces4);
+console.log(enlaces3);
