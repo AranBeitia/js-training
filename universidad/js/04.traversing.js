774f13b0c51a44588bd55b4ce3675b9f3292327d
AranBeitia <arantza.beitia@gmail.com>
2019-09-30 08:55:58 +0200

    Training

--- universidad/js/04.traversing.js
+++ universidad/js/04.traversing.js
@@ -1,11 +1,12 @@
 //TRAVERSING DE PADRE A HIJO
+const navegacion = document.querySelector("#principal");
 
-// const nav = document.querySelector("#principal");
-
-// console.log(nav.nodeName); //los nodos son las etiquetad HTML
-// console.log(nav.children[0].nodeName);
-// console.log(nav.children[0].nodeType);
-// console.log((nav.children[0].textContent = "enlace ss"));
+console.log(navegacion.nodeName);//los nodos son las etiquetas HTMl
+console.log(navegacion.childNodes);
+console.log(navegacion.children);
+console.log(navegacion.children[0].nodeName);
+console.log(navegacion.nodeName);
+console.log(navegacion.children[0].textContent = "enlaces sss");
 
 // /**************
 // * nodeType devuelve numeros:
@@ -17,33 +18,30 @@
 //   10 = doctype
 // ***************/
 
-// const barra = document.querySelector(".barra");
-// //podemos acceder todo lo profundo que queramos en el nodo
-// console.log(barra.children[0].children[0].children);
+const barra = document.querySelector(".barra");
+//podemos acceder todo lo profundo que queramos en el nodo
+console.log(barra.children[0].children[0].children);
 
-// const card = document.querySelectorAll(".card");
-// console.log(card[0].lastChild);
-// console.log(card[0].lastElementChild);
-// console.log(card[0].firstElementChild);
+const card = document.querySelectorAll(".card");
+console.log(card[0].lastChild);
+console.log(card[0].lastElementChild);
+console.log(card[0].firstElementChild);
 
-// //TRAVERSING DE HIJO A PADRE
+//TRAVERSING DE HIJO A PADRE
+const enlaces = document.querySelectorAll(".enlace");
+//elemento padre del nodo
+console.log(enlaces[0].parentNode);
+console.log(enlaces[0].parentElement); //recomendado
 
-// const enlaces = document.querySelectorAll(".enlace");
-// //elemento padre del nodo
-// console.log(enlaces[0].parentNode);
-// console.log(enlaces[0].parentElement); //recomendado
-// //podemos subir todo lo que queramos
-// console.log(enlaces[0].parentElement.parentElement);
+//podemos subir todo lo que queramos
+console.log(enlaces[0].parentElement.parentElement);
 
-// const cursos = document.querySelectorAll(".card");
-// console.log(
-//   (cursos[0].parentElement.parentElement.parentElement.children[0].textContent =
-//     "Kitty satanasa")
-// );
+const cursos = document.querySelectorAll(".card");
+console.log(cursos[0].parentElement.parentElement.parentElement.children[0].textContent = "hola desde traversing");
 
-// //SIBILING
-// const enlaces2 = document.querySelectorAll('.enlace');
-// //recorre los hermanos del nodo
-// console.log(enlaces2[4].previousSibling);//devuelve espacio en blanco
-// console.log(enlaces2[4].previousElementSibling.previousElementSibling);
-// console.log(enlaces2[2].nextElementSibling.nextElementSibling.parentElement.children[4]);
+//SIBILING
+const links = document.querySelectorAll('.enlace');
+//recorre los hermanos del nodo
+console.log(enlaces[4].previousSibling);//devuelve espacio en blanco
+console.log(enlaces[4].previousElementSibling.previousElementSibling);
+console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement.children[4]);
