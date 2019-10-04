2b237b2efc198458007fcc910f5db9f9e1663b6f
AranBeitia <arantza.beitia@gmail.com>
2019-10-04 09:17:50 +0200

    Training

--- 03.PROYECTO-1/formularioLS/assets/js/app.js
+++ 03.PROYECTO-1/formularioLS/assets/js/app.js
@@ -1,45 +1,44 @@
-// //(video 61)
-// /*** Variables ***/
-// const listaTweets = document.querySelector("#lista-tweets");
+/*** Variables ***/
+const listaTweets = document.querySelector('#lista-tweets');
 
-// /*** Event Listeners ***/
-// eventListeners();
+/*** Event Listeners ***/
+eventListeners();
 
-// function eventListeners() {
-//   //Cuando se envía el formulario
-//   document
-//     .getElementById("formulario")
-//     .addEventListener("submit", agregarTweet);
+function eventListeners() {
+	//Cuando se envía el formulario
+	document.querySelector('#formulario').addEventListener('submit', agregarTweet);
 
-//     //Borrar tweets
-//     listaTweets.addEventListener('click', borrarTweet);
-// }
+	//borrar tweets
+	listaTweets.addEventListener('click', borrarTweet);
+}
 
-// /*** Funciones ***/
-// //Añadir tweet del formulario
-// function agregarTweet(e) {
-//   e.preventDefault();
-//   //leer el valor del textarea
-//   const tweet = document.getElementById('tweet').value;
-//   //crear boton borrar
-//   const botonBorrar = document.createElement('a');
-//   botonBorrar.classList = 'borrar-tweet';
-//   botonBorrar.innerText = 'X';
+/*** Funciones ***/
+//añade tweet del formulario
+function agregarTweet(e) {
+	e.preventDefault();
+	//leer el valor del textarea
+	const tweet = document.querySelector('#tweet').value;
+	//crear boton de eliminar tweet
+	const botonBorrar = document.createElement('a');
+	botonBorrar.classList = 'borrar-tweet';
+	botonBorrar.innerText = 'X';
 
-//   //crear elemento y añadirle el contenido a la lista
-//   const li = document.createElement('li');
-//   li.innerText = tweet;
-//   //añade el boton de borrar al tweet
-//   li.appendChild(botonBorrar);
-//   //añade el tweet a la lista
-//   listaTweets.appendChild(li);
-//   //console.log(tweet);
-// }
+	//crear elemento y añadirle el contenido a la lista
+	const li = document.createElement('li');
+	li.innerText = tweet;
+	//añade el boton de borrar el tweet
+	li.appendChild(botonBorrar);
+	//añade el tweet a la lista
+	listaTweets.appendChild(li);
 
-// function borrarTweet(e){
-//   e.preventDefault();
-//   if(e.target.className === 'borrar-tweet') {
-//     console.log(e.target.parentElement.remove());
-//     alert('Tweet eliminado');
-//   } 
-// }
+	console.log(listaTweets);
+}
+
+//borrar tweet
+function borrarTweet(e) {
+	e.preventDefault();
+	if(e.target.className === 'borrar-tweet') {
+		console.log(e.target.parentElement.remove());
+		alert('Tweet eliminado');
+	}
+}
