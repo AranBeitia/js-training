855b05a0dc234762d17a60afd731ec3946a67478
AranBeitia <arantza.beitia@gmail.com>
2019-10-04 10:56:07 +0200

    training

--- 03.PROYECTO-1/formularioLS/assets/js/app.js
+++ 03.PROYECTO-1/formularioLS/assets/js/app.js
@@ -31,14 +31,36 @@ function agregarTweet(e) {
 	//añade el tweet a la lista
 	listaTweets.appendChild(li);
 
-	console.log(listaTweets);
+	//añadir a local storage
+	agregarTweetLocalStorage(tweet);
 }
 
-//borrar tweet
+//borrar tweet del DOM
 function borrarTweet(e) {
 	e.preventDefault();
 	if(e.target.className === 'borrar-tweet') {
 		console.log(e.target.parentElement.remove());
 		alert('Tweet eliminado');
 	}
+}
+
+//agregar tweet a local storage
+function agregarTweetLocalStorage(tweet) {
+	let tweets;
+	tweets = obtenerTweetLocalStorage();
+	//Añadir el nuevo tweet
+	tweets.push(tweet);
+	//convertir de string a array para local storage
+	localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+function obtenerTweetLocalStorage() {
+	let tweets;
+	//revisamos los valores de local storage
+	if(localStorage.getItem('tweets') === null) {
+		tweets = [];
+	} else {
+		tweets = JSON.parse(localStorage.getItem('tweets'));
+	}
+	return tweets;
 }
