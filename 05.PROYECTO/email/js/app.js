5f9a77b3edf872530f064a32afb2533790e58f38
AranBeitia <arantza.beitia@gmail.com>
2019-11-06 08:32:34 +0100

    Trainin form

--- 05.PROYECTO/email/js/app.js
+++ 05.PROYECTO/email/js/app.js
@@ -1,98 +1,110 @@
-//Variables
-const email = document.getElementById('email');
-const asunto = document.getElementById('asunto');
-const mensaje = document.getElementById('mensaje');
-const btnEnviar = document.getElementById('enviar');
-const formularioEnviar = document.getElementById('enviar-mail');
-const resetBtn = document.getElementById('resetBtn');
-//Event listeners
-eventListeners();
+// Variables
 
-function eventListeners() {
-	//Inicio de la aplicación y deshabilitar submit
-	document.addEventListener('DOMContentLoaded', inicioApp);
-	//Campos del formulario
-	email.addEventListener('blur', validarCampo);
-	asunto.addEventListener('blur', validarCampo);
-	mensaje.addEventListener('blur', validarCampo);
-	//boton enviar en el submit
-	formularioEnviar.addEventListener('submit', enviarEmail);
-	//boton de reset
-	resetBtn.addEventListener('click', resetFormulario);
-}
+// Event listeners
 
-//Funciones
-function inicioApp() {
-	//deshabilitar el envío
-	btnEnviar.disabled = true;
-	console.log(btnEnviar);
-}
-//valida que el campo tenga algo escrito
-function validarCampo() {
-	//se valda la longitud del texto
-	validarLongitud(this);
+// Funciones
 
-	//validar unicamente el email
-	if(this.type === 'email') {
-		validarEmail(this);
-	}
-	console.log(this.type);
 
-	let errores = document.querySelectorAll('.error');
-	if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
-		if (errores.length === 0) {
-			btnEnviar.disabled = false;
-		}
-	}
-	//console.log('Dentro del INPUT');
-}
-//resetear el formulario
-function resetFormulario(e) {
-	formularioEnviar.reset();
-	e.preventDefault();
-}
 
-//cuando se envia el email
-function enviarEmail(e) {
-	//spinner al presionar enviar
-	const spinnerGif = document.querySelector('#spinner');
-	spinnerGif.style.display = 'block';
-	//gif que envia email
-	const enviado = document.createElement('img');
-	enviado.src = 'img/mail.gif';
-	enviado.style.display = 'block';
-	//ocultar spinner y mostrar gif enviado
-	setTimeout(function() {
-		spinnerGif.style.display = 'none';
-		document.querySelector('#loaders').appendChild(enviado);
-		setTimeout(function(){
-			enviado.remove();
-			formularioEnviar.reset();
-		}, 5000);
-	}, 3000);
 
-	e.preventDefault();
 
-}
-//verifica la longitud del texto en los campos
-function validarLongitud(campo) {
-	if (campo.value.length > 0) {
-		campo.style.borderBottomColor = 'green';
-		campo.classList.remove('error');
-	} else {
-		campo.style.borderBottomColor = 'red';
-		campo.classList.add('error');
-	}
-	//console.log(campo);
-}
 
-function validarEmail(campo) {
-	const mensaje = campo.value;
-	if(mensaje.indexOf('@') !== -1) {
-		campo.style.borderBottomColor = 'green';
-		campo.classList.remove('error');
-	} else {
-		campo.style.borderBottomColor = 'red';
-		campo.classList.add('error');
-	}
-}
+
+// //Variables
+// const email = document.getElementById('email');
+// const asunto = document.getElementById('asunto');
+// const mensaje = document.getElementById('mensaje');
+// const btnEnviar = document.getElementById('enviar');
+// const formularioEnviar = document.getElementById('enviar-mail');
+// const resetBtn = document.getElementById('resetBtn');
+// //Event listeners
+// eventListeners();
+
+// function eventListeners() {
+// 	//Inicio de la aplicación y deshabilitar submit
+// 	document.addEventListener('DOMContentLoaded', inicioApp);
+// 	//Campos del formulario
+// 	email.addEventListener('blur', validarCampo);
+// 	asunto.addEventListener('blur', validarCampo);
+// 	mensaje.addEventListener('blur', validarCampo);
+// 	//boton enviar en el submit
+// 	formularioEnviar.addEventListener('submit', enviarEmail);
+// 	//boton de reset
+// 	resetBtn.addEventListener('click', resetFormulario);
+// }
+
+// //Funciones
+// function inicioApp() {
+// 	//deshabilitar el envío
+// 	btnEnviar.disabled = true;
+// 	console.log(btnEnviar);
+// }
+// //valida que el campo tenga algo escrito
+// function validarCampo() {
+// 	//se valda la longitud del texto
+// 	validarLongitud(this);
+
+// 	//validar unicamente el email
+// 	if(this.type === 'email') {
+// 		validarEmail(this);
+// 	}
+// 	console.log(this.type);
+
+// 	let errores = document.querySelectorAll('.error');
+// 	if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+// 		if (errores.length === 0) {
+// 			btnEnviar.disabled = false;
+// 		}
+// 	}
+// 	//console.log('Dentro del INPUT');
+// }
+// //resetear el formulario
+// function resetFormulario(e) {
+// 	formularioEnviar.reset();
+// 	e.preventDefault();
+// }
+
+// //cuando se envia el email
+// function enviarEmail(e) {
+// 	//spinner al presionar enviar
+// 	const spinnerGif = document.querySelector('#spinner');
+// 	spinnerGif.style.display = 'block';
+// 	//gif que envia email
+// 	const enviado = document.createElement('img');
+// 	enviado.src = 'img/mail.gif';
+// 	enviado.style.display = 'block';
+// 	//ocultar spinner y mostrar gif enviado
+// 	setTimeout(function() {
+// 		spinnerGif.style.display = 'none';
+// 		document.querySelector('#loaders').appendChild(enviado);
+// 		setTimeout(function(){
+// 			enviado.remove();
+// 			formularioEnviar.reset();
+// 		}, 5000);
+// 	}, 3000);
+
+// 	e.preventDefault();
+
+// }
+// //verifica la longitud del texto en los campos
+// function validarLongitud(campo) {
+// 	if (campo.value.length > 0) {
+// 		campo.style.borderBottomColor = 'green';
+// 		campo.classList.remove('error');
+// 	} else {
+// 		campo.style.borderBottomColor = 'red';
+// 		campo.classList.add('error');
+// 	}
+// 	//console.log(campo);
+// }
+
+// function validarEmail(campo) {
+// 	const mensaje = campo.value;
+// 	if(mensaje.indexOf('@') !== -1) {
+// 		campo.style.borderBottomColor = 'green';
+// 		campo.classList.remove('error');
+// 	} else {
+// 		campo.style.borderBottomColor = 'red';
+// 		campo.classList.add('error');
+// 	}
+// }
