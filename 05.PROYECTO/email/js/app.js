649257e84ac658e99a4f7106e5dbb5241eb1a1d3
AranBeitia <arantza.beitia@gmail.com>
2019-12-02 14:22:07 +0100

    training

--- 05.PROYECTO/email/js/app.js
+++ 05.PROYECTO/email/js/app.js
@@ -4,24 +4,65 @@ const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
 
-
-
-
 // Event listeners
 eventListeners();
 
 function eventListeners() {
 	//Inicio de la aplicacion y deshabilitar submit
 	document.addEventListener('DOMContentLoaded', inicioApp);
+
+	//campos del formulario
+	email.addEventListener('blur', validarCampo);
+	asunto.addEventListener('blur', validarCampo);
+	mensaje.addEventListener('blur', validarCampo);
 }
-
-
 
 // Funciones
 function inicioApp() {
 	//deshabilitar el envío
 	btnEnviar.disabled = true;
-	console.log('btnEnviar');
+}
+
+//Valida que el campo tenga algo escrito
+function validarCampo() {
+
+	//se valida la longitud del texto y que no esté vacio
+	validarLongitud(this);
+
+	// validar unicamente el email
+	if (this.type === 'email') {
+		validarEmail(this);
+	}
+
+	let errores = document.querySelectorAll('.error');
+	if(email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+		if(errores.length === 0) {
+			btnEnviar.disabled = false;
+		}
+	}
+}
+
+function validarLongitud(campo) {
+	console.log(campo.value.length);
+
+	if (campo.value.length > 0) {
+		campo.style.borderBottomColor = 'green';
+		campo.classList.remove('error');
+	} else {
+		campo.style.borderBottomColor = 'red';
+		campo.classList.add('error');
+	}
+}
+
+function validarEmail(campo) {
+	const mensaje = campo.value;
+	if(mensaje.indexOf('@') !== -1) {
+		campo.style.borderBottomColor = 'green';
+		campo.classList.remove('error');
+	} else {
+		campo.style.borderBottomColor = 'red';
+		campo.classList.add('error');
+	}
 }
 
 
