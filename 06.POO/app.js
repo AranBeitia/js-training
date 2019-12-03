c8ce2bb2f4dd57d80507e9556b23b48ba94bfabb
AranBeitia <arantza.beitia@gmail.com>
2019-12-03 09:18:16 +0100

    POO

---
+++ 06.POO/app.js
@@ -0,0 +1,42 @@
+//object literal
+const cliente = {
+	nombre: 'Juan',
+	saldo: 200,
+	tipoCliente: function () {
+		let tipo;
+
+		if (this.saldo > 1000) {
+			tipo = 'Gold';
+		} else if (this.saldo > 500) {
+			tipo = 'Platinum';
+		} else {
+			tipo = 'Normal';
+		}
+		return tipo;
+	}
+}
+console.log(cliente.tipoCliente());
+console.log(cliente.saldo);
+
+//metodo alternativo
+function Cliente(nombre, saldo) {
+	this.nombre = nombre;
+	this.saldo = saldo;
+	this.tipoCliente = function () {
+		let tipo;
+
+		if (this.saldo > 1000) {
+			tipo = 'Gold';
+		} else if (this.saldo > 500) {
+			tipo = 'Platinum';
+		} else {
+			tipo = 'Normal';
+		}
+		return tipo;
+	}
+}
+
+const persona = new Cliente('Pedro', 20000);
+const persona2 = new Cliente('Karen', 6000);
+
+console.log(persona2.tipoCliente());
