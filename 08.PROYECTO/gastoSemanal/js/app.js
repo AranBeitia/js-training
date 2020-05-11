37c56f1d67ab62845eb1f27fe4c442afe43f5123
AranBeitia <arantza.beitia@gmail.com>
2020-05-11 14:25:58 +0200

    Training

--- 08.PROYECTO/gastoSemanal/js/app.js
+++ 08.PROYECTO/gastoSemanal/js/app.js
@@ -1,82 +1,122 @@
 // Variables
-const presupuestoUsuario = prompt('Cual es tu presupuesto Semanal?');
+const presupuestoUsuario = prompt('Cual es tu presupuesto semanal?');
 const formulario = document.getElementById('agregar-gasto');
-
 let cantidadPresupuesto;
 
 
 // Clases
-//clase de presupuesto
 class Presupuesto {
 	constructor(presupuesto) {
 		this.presupuesto = Number(presupuesto);
 		this.restante = Number(presupuesto);
 	}
-	//metodo para ir restando del presupuesto actual
-	presupuestoRestante(cantidad = 0) {
+	// Metodo para ir restando el presupuesto actual
+	presupuestoRestante(cantidad = 0){
 		return this.restante -= Number(cantidad);
 	}
 }
-//clase de interfaz maneja todo lo relacionado a el HTML
+//Clase de interfaz maneja todo lo relacionado con el HTML
 class Interfaz {
 	insertarPresupuesto(cantidad) {
 		const presupuestoSpan = document.querySelector('span#total');
 		const restanteSpan = document.querySelector('span#restante');
 
-		//insertat al HTML
+		//Insertar HTML
 		presupuestoSpan.innerHTML = `${cantidad}`;
 		restanteSpan.innerHTML = `${cantidad}`;
 	}
 
-	imprimirMensaje(mensaje, tipo) {
+	imprimirMensaje(mensaje, tipo){
 		const divMensaje = document.createElement('div');
 		divMensaje.classList.add('text-center', 'alert');
-
-		if(tipo === 'error') {
+		if(tipo === 'error'){
 			divMensaje.classList.add('alert-danger');
-		} else {
+		}else{
 			divMensaje.classList.add('alert-success');
 		}
 		divMensaje.appendChild(document.createTextNode(mensaje));
-		//insertar en el DOM
+		//Insertar en el DOM
 		document.querySelector('.primario').insertBefore(divMensaje, formulario);
-		//quitar el alert después de 3 segundos
 		setTimeout(function(){
 			document.querySelector('.primario .alert').remove();
 			formulario.reset();
 		}, 3000);
 	}
+
+	// Inserta los gastos a la lista
+	agregarGastoListado(nombre, cantidad) {
+		const gastosListado = document.querySelector('#gastos ul');
+		//Crear un li
+		const li = document.createElement('li');
+		li.className = 'list-group-item d-flex justify-content-between align-items-center';
+		//Insertar el gasto
+		li.innerHTML = `
+		${nombre}
+		<span class="badge badge-primary badge-pill">$ ${cantidad}</span>
+		`;
+		//Insertar al HTML
+		gastosListado.appendChild(li);
+	}
+
+	//Comprueba el presupuesto restante
+	presupuestoRestante(cantidad){
+		const restante = document.querySelector('span#restante');
+		//leemos el presupuesto restante
+		const presupuestoRestanteUsuario = cantidadPresupuesto.presupuestoRestante(cantidad);
+		restante.innerHTML = `${presupuestoRestanteUsuario}`;
+		this.comprobarPresupuesto();
+	}
+
+	//cambia de color el presupuesto restante
+	comprobarPresupuesto() {
+		const presupuestoTotal = cantidadPresupuesto.presupuesto;
+		const presupuestoRestante = cantidadPresupuesto.restante;
+
+		//Compobar el 25% del gasto
+		if (presupuestoTotal / 4 > presupuestoRestante) {
+			const restante = document.querySelector('.restante');
+			restante.classList.remove('alert-success', 'alert-warning');
+			restante.classList.add('alert-danger');
+		} else if(presupuestoTotal / 2 > presupuestoRestante) {
+			const restante = document.querySelector('.restante');
+			restante.classList.remove('alert-success');
+			restante.classList.add('alert-warning');
+		}
+
+	}
 }
 
-//Event listeners
+// Event listeners
 document.addEventListener('DOMContentLoaded', function(){
-	if(presupuestoUsuario === null || presupuestoUsuario === '') {
+	if(presupuestoUsuario == null || presupuestoUsuario == ''){
 		window.location.reload();
 	} else {
 		//Instanciar un presupuesto
 		cantidadPresupuesto = new Presupuesto(presupuestoUsuario);
-		//instanciar la clase de interfaz
+		//Instanciar la clase Interfaz
 		const ui = new Interfaz();
 		ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
 
 	}
 });
 
-formulario.addEventListener('submit', function(e) {
+formulario.addEventListener('submit', function(e){
 	e.preventDefault();
-
+	
 	//leer del formulario de gastos
 	const nombreGasto = document.querySelector('#gasto').value;
 	const cantidadGasto = document.querySelector('#cantidad').value;
 
 	//instanciar la interfaz
 	const ui = new Interfaz();
-	//comprobar que los campos no estén vacíos
-	if(nombreGasto === '' || cantidadGasto === '') {
+
+	//Comprobar que los campos no estan vacios
+	if (nombreGasto === '' || cantidadGasto === '') {
 		//2 parametros: mensaje y tipo
-		ui.imprimirMensaje('ha habido un error', 'error');
-		console.log('ha habido un error');
-	} else {
-		console.log('El gasto se ha agregado');
+		ui.imprimirMensaje('Hubo un error', 'error');
+	}else{
+		ui.imprimirMensaje('el gasto se agregó','correcto');
+		ui.agregarGastoListado(nombreGasto, cantidadGasto);
+		ui.presupuestoRestante(cantidadGasto);
 	}
 });
