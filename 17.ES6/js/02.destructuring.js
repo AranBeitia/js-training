f8a45f065d8d6875a1c96d3c77497a6c06191870
AranBeitia <arantza.beitia@gmail.com>
2020-05-25 11:00:15 +0200

    training

---
+++ 17.ES6/js/02.destructuring.js
@@ -0,0 +1,40 @@
+//Destructing a arrays
+
+const ciudades = ['Londres', 'New York', 'Madrid', 'Paris', {
+	idioma: 'ingles'
+}];
+
+const [
+	primeraCiudad,
+	segundaCiudad
+] = ciudades;
+
+console.log(primeraCiudad, segundaCiudad);
+
+const [ , , ciudad , paris ] = ciudades;
+console.log(ciudad, paris);
+
+console.log(ciudades);
+
+//ejemplo avanzado
+const cliente = {
+	tipo: 'premium',
+	saldo: 30000,
+	datos: {
+		nombre: 'Pedro',
+		apellido: 'Perez',
+		residencia: {
+			ciudad: 'Mexico'
+		}
+	},
+	movimientos: ['12-03-2018', '12-03-2017', '12-03-2016']
+}
+
+let {
+	tipo,
+	datos,
+	datos: {residencia},
+	movimientos : [ , dos]
+} = cliente;
+
+console.log(tipo, datos.apellido, residencia, dos);
