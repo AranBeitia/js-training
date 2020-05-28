f06dd6579c861b66263de6e602a8232ad170c0d5
AranBeitia <arantza.beitia@gmail.com>
2020-05-28 11:17:52 +0200

    training

---
+++ 19.MODULOS/js/app.js
@@ -0,0 +1,11 @@
+import {nombreCliente, ahorro} from './cliente.js';
+
+console.log(nombreCliente)
+console.log(ahorro)
+
+import * as cliente from './cliente.js'; 
+console.log(cliente)
+
+const info = mostrarInformacion(nombreCliente, ahorro)
+
+console.log(info)
---
+++ 19.MODULOS/js/cliente.js
@@ -0,0 +1,6 @@
+export const nombreCliente = 'Aran';
+export let ahorro = 200;
+
+function mostrarInformacion(nombre, ahorro) {
+	return `Cliente ${nombre} - Ahorro ${ahorro}`
+}
