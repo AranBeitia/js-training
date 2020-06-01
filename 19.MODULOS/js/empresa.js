f056c3ce0451524bdbbbf436e643d6f356a00bd1
AranBeitia <arantza.beitia@gmail.com>
2020-06-01 13:57:57 +0200

    training

--- 19.MODULOS/js/empresa.js
+++ 19.MODULOS/js/empresa.js
@@ -1,3 +1,5 @@
+import { Cliente } from './cliente.js';
+
 //exportar variables
 export const nombreEmpresa = 'Init Services';
 export let ahorro = 2000000000000;
@@ -6,4 +8,16 @@ export const categoria = 'Aprendizaje';
 //exportar funciones
 export function mostrarInformacion(nombre, ahorro, categoria) {
 	return `Cliente ${nombre} - Ahorro ${ahorro} - Categoria: ${categoria}`
+}
+
+//utilizar la clase del modulo cliente
+export class Empresa extends Cliente {
+	constructor(nombre, ahorro, categoria) {
+		super(nombre, ahorro);
+		this.categoria = categoria;
+	}
+
+	mostrarInformacion() {
+		return `Cliente ${this.nombre} - Ahorro ${this.ahorro} - Categoria: ${this.categoria}`
+	}
 }
