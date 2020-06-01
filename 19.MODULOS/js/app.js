a8932f4328fcb3b1329ac3ecdac200e97fb9bdb5
AranBeitia <arantza.beitia@gmail.com>
2020-06-01 12:58:23 +0200

    training

--- 19.MODULOS/js/app.js
+++ 19.MODULOS/js/app.js
@@ -1,11 +1,12 @@
-import {nombreCliente, ahorro} from './cliente.js';
+// import { nombreCliente, ahorro, mostrarInformacion, Cliente } from './cliente.js';
+import * as clientes from './cliente.js';
 
-console.log(nombreCliente)
-console.log(ahorro)
+// console.log(nombreCliente)
+// console.log(ahorro)
 
-import * as cliente from './cliente.js'; 
-console.log(cliente)
-
-const info = mostrarInformacion(nombreCliente, ahorro)
-
+const info = clientes.mostrarInformacion(clientes.nombreCliente, clientes.ahorro) //funcion local a este archivo
 console.log(info)
+
+//utilizar la clase
+let cliente = new clientes.Cliente(clientes.nombreCliente, clientes.ahorro);
+console.log(cliente.mostrarInformacion());
