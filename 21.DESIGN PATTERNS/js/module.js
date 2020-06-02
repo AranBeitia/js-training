ac14fbf7f006345bac9d0c7b340432de6e7f1134
AranBeitia <arantza.beitia@gmail.com>
2020-06-02 13:28:43 +0200

    training

--- 21.DESIGN PATTERNS/js/module.js
+++ 21.DESIGN PATTERNS/js/module.js
@@ -3,15 +3,44 @@ const comprarBoleto = (function () {
 	let evento = 'Conferencia JS 2020';
 	let precio = 200;
 
+	const adquirirBoleto = () => {
+		const elemento = document.createElement('p');
+		elemento.textContent = `Comprado boleto para: ${evento}`;
+		document.querySelector('#app').appendChild(elemento);
+	}
+
 	//publico
 	return {
 		mostrarBoleto: function(){
-			console.log(evento)
+			adquirirBoleto();
+			// console.log(evento);
+			// console.log(precio);
 		}
 	}
 })();
 
 comprarBoleto.mostrarBoleto();
+console.log(comprarBoleto.evento);
 
 //no está definida la variable porque es privada (encima del return)
-console.log(precio);
+// console.log(precio);
+
+const comprarEntrada = (function() {
+	//privado
+	let evento = 'Conferencias CSS 2020';
+	const adquirirEntrada = () => {
+		const item = document.createElement('p');
+		item.textContent = `Comprando entrada para: ${evento}`;
+		document.querySelector('#app').appendChild(item);
+	}
+
+	//publico
+	return {
+		mostrarEntrada: function(){
+			adquirirEntrada()
+		}
+	}
+})();
+
+comprarEntrada.mostrarEntrada();
+console.log(comprarEntrada.evento)
