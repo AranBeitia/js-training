bcc42060b4cba26b7aa5ba7ac315d53c6a34d8c7
AranBeitia <arantza.beitia@gmail.com>
2020-08-05 09:44:47 +0200

    training

--- JS-2/14.CALCULADORA/scripts.js
+++ JS-2/14.CALCULADORA/scripts.js
@@ -1,3 +1,6 @@
+var operator = null
+var inputValueMemo = 0
+
 //coger el valor
 function getContentClick (event) {
   const value = event.target.innerHTML
@@ -7,15 +10,63 @@ function getContentClick (event) {
 //tela de araña que manda cada funcion a su sitio
 const filterAction= value => { 
   value === '0' ? addValueInput(0) : null
+  value === '1' ? addValueInput(1) : null
+  value === '2' ? addValueInput(2) : null
+  value === '3' ? addValueInput(3) : null
+  value === '4' ? addValueInput(4) : null
+  value === '5' ? addValueInput(5) : null
+  value === '6' ? addValueInput(6) : null
+  value === '7' ? addValueInput(7) : null
+  value === '8' ? addValueInput(8) : null
+  value === '9' ? addValueInput(9) : null
+  value === ',' ? addValueInput(',') : null
+
+  value === '+' ? setOperation('+') : null
+  value === '-' ? setOperation('-') : null
+  value === 'X' ? setOperation('*') : null
+  value === '/' ? setOperation('/') : null
+  value === '%' ? setOperation('%') : null
+  value === '+/-' ? setOperation('+/-') : null
 }
 
 function addValueInput (value) {
   const inputScreen = document.getElementsByClassName('calculator__screen')[0]
-  inputScreen.value = value
+  const inputValue = inputScreen.value
+
+  if(inputValue === '0' && inputValue.length === 1 && value !== ',') {
+    inputScreen.value = value
+    return
+  }
+  inputScreen.value = inputValue + value
+}
+
+function setOperation (operator) {
+  const inputScreenvalue = document.getElementsByClassName('calculator__screen')[0]
+  this.operator = operator
+
+  if(inputScreenvalue != 0) {
+    calculation()
+  }
+}
+
+function calculation() {
+  const inputScreen = document.getElementsByClassName('calculator__screen')[0]
+  let valueOne = this.inputValueMemo
+  let valueTwo = inputScreen.value
+  let total = 0
+
+  if(this.operator === '+' && inputScreen.value !== '') {
+    total = valueOne + valueTwo
+  }
+  this.inputValueMemo = total
+  inputScreen.value = ''
+  inputScreen.placeholder = total
 }
 
 
 
+
+/**/
 function presionarBoton () {
   console.log('presiona boton');
 }
@@ -33,5 +84,5 @@ function resultado () {
 }
 
 function borrar () {
-
+  
 }
