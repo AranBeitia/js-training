3327b634b648c8c6e700d933f59b2cf3915010a5
AranBeitia <arantza.beitia@gmail.com>
2020-09-02 10:54:59 +0200

    training

--- JS-2/15.CARRITO/js/scripts.js
+++ JS-2/15.CARRITO/js/scripts.js
@@ -1,7 +1,8 @@
-const CARD_PRODUCTS = 'cartProductsId'
+const CARD_PRODUCTS = 'cart-Products-Id'
 
 document.addEventListener('DOMContentLoaded', () => {
   loadProducts()
+  loadProductCard()
 })
 
 function getProductsDb() {
@@ -30,7 +31,7 @@ async function loadProducts() {
           <h5 class="card-title">${product.name}</h5>
           <p class="card-text">${product.extraInfo}</p>
           <p class="card-text">${product.price} € / Unidad</p>
-          <button type="button" class="btn btn-primary btn-cart" onClick="idProduct(${product.id})">Añadir al carrito</button>
+          <button type="button" class="btn btn-primary btn-cart" onClick="addProduct(${product.id})">Añadir al carrito</button>
         </div>
       </div>
     </div>
@@ -54,7 +55,7 @@ function openCloseCart() {
   console.log(containerCart);
 }
 
-function idProduct(idProduct) {
+function addProduct(idProduct) {
   let arrayProductId = []
   let localStorageItems = localStorage.getItem(CARD_PRODUCTS)
 
@@ -64,11 +65,67 @@ function idProduct(idProduct) {
   } else {
     let productsId = localStorage.getItem(CARD_PRODUCTS)
     if(productsId.length > 0) {
-      productsId += ',' + idProduct
+      productsId += ', ' + idProduct
     } else {
       productsId = productId
     }
     localStorage.setItem(CARD_PRODUCTS, productsId)
   }
-  console.log(localStorageItems);
+  loadProductCard()
+}
+
+async function loadProductCard () {
+  const products = await getProductsDb()
+  //convetir resultado del localStorage en un array
+  const localStorageItems = localStorage.getItem(CARD_PRODUCTS)
+  let html = ''
+  if (!localStorageItems) {
+    html = `
+      <div class="cart-product empty">
+        <p>Carrito vacío</p>
+      </div>
+    `
+  } else {
+    const idProductsSplit = localStorageItems.split(', ')
+
+    //eliminar los id duplicados
+    const idProductCard = Array.from(new Set(idProductsSplit))
+
+    idProductCard.forEach(id => {
+      products.forEach(product => {
+        if(id == product.id) {
+          const quantity = countDuplicatesId(id, idProductsSplit)
+          const totalPrice = product.price * quantity
+          html += `
+            <div class="cart-product">
+              <img src="${product.image}" alt="${product.name}">
+              <div class="cart-product-info">
+                <span class="quantity">${quantity}</span>
+                <p>${product.name}</p>
+                <p>${totalPrice.toFixed(2)}</p>
+                <div class="change-quantity">
+                  <button>-</button>
+                  <button>+</button>
+                </div>
+                <div class="cart-product-delete">
+                  <button>Eliminar</button>
+                </div>
+              </div>
+            </div>
+          `
+        }
+      })
+    })
+  }
+  document.getElementsByClassName('cart-products')[0].innerHTML = html
+}
+
+function countDuplicatesId(value, arrayIds) {
+  let count = 0
+  arrayIds.forEach(id => {
+    if(value == id) {
+      count++
+    }
+  })
+  return count
 }
