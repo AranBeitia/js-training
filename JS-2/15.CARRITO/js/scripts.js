1f91ef574f57c3fd232e74631f9ef6ab858fbbaa
AranBeitia <arantza.beitia@gmail.com>
2020-10-27 08:58:26 +0100

    training

--- JS-2/15.CARRITO/js/scripts.js
+++ JS-2/15.CARRITO/js/scripts.js
@@ -1,26 +1,19 @@
-const CARD_PRODUCTS = 'cart-Products-Id'
+const CARD_PRODUCTS = "cartProductsId"
 
 document.addEventListener('DOMContentLoaded', () => {
   loadProducts()
-  loadProductCard()
 })
 
-function getProductsDb() {
+function getProductsDB() {
   const url = './dbProducts.json'
   return fetch(url)
-  .then(response => {
-    return response.json()
-  })
-  .then(result => {
-    return result
-  })
-  .catch(err => {
-    console.log(err);
-  })
+  .then(response =>  response.json())
+  .then(result =>  result)
+  .catch(err => console.log(err))
 }
 
-async function loadProducts() {
-  const products = await getProductsDb()
+async function loadProducts () {
+  const products = await getProductsDB()
   let html = ''
   products.forEach(product => {
     html += `
@@ -40,92 +33,126 @@ async function loadProducts() {
   document.getElementsByClassName('products')[0].innerHTML = html
 }
 
-function openCloseCart() {
-  const containerCart = document.getElementsByClassName('cart-products')[0]
-  containerCart.classList.forEach(item => { 
-    if(item === 'hidden') {
-      containerCart.classList.remove('hidden')
-      containerCart.classList.add('active')
-    }
-    if (item === 'active') {
-      containerCart.classList.remove('active')
-      containerCart.classList.add('hidden')
-    }
-   })
-  console.log(containerCart);
+function openCloseCart () {
+  const btnCart = document.getElementsByClassName('cart-products')[0]
+  console.log(btnCart);
 }
 
-function addProduct(idProduct) {
-  let arrayProductId = []
-  let localStorageItems = localStorage.getItem(CARD_PRODUCTS)
 
-  if(localStorageItems === null) {
-    arrayProductId.push(idProduct)
-    localStorage.setItem(CARD_PRODUCTS, arrayProductId)
-  } else {
-    let productsId = localStorage.getItem(CARD_PRODUCTS)
-    if(productsId.length > 0) {
-      productsId += ', ' + idProduct
-    } else {
-      productsId = productId
-    }
-    localStorage.setItem(CARD_PRODUCTS, productsId)
-  }
-  loadProductCard()
-}
 
-async function loadProductCard () {
-  const products = await getProductsDb()
-  //convetir resultado del localStorage en un array
-  const localStorageItems = localStorage.getItem(CARD_PRODUCTS)
-  let html = ''
-  if (!localStorageItems) {
-    html = `
-      <div class="cart-product empty">
-        <p>Carrito vacío</p>
-      </div>
-    `
-  } else {
-    const idProductsSplit = localStorageItems.split(', ')
 
-    //eliminar los id duplicados
-    const idProductCard = Array.from(new Set(idProductsSplit))
 
-    idProductCard.forEach(id => {
-      products.forEach(product => {
-        if(id == product.id) {
-          const quantity = countDuplicatesId(id, idProductsSplit)
-          const totalPrice = product.price * quantity
-          html += `
-            <div class="cart-product">
-              <img src="${product.image}" alt="${product.name}">
-              <div class="cart-product-info">
-                <span class="quantity">${quantity}</span>
-                <p>${product.name}</p>
-                <p>${totalPrice.toFixed(2)}</p>
-                <div class="change-quantity">
-                  <button>-</button>
-                  <button>+</button>
-                </div>
-                <div class="cart-product-delete">
-                  <button>Eliminar</button>
-                </div>
-              </div>
-            </div>
-          `
-        }
-      })
-    })
-  }
-  document.getElementsByClassName('cart-products')[0].innerHTML = html
-}
 
-function countDuplicatesId(value, arrayIds) {
-  let count = 0
-  arrayIds.forEach(id => {
-    if(value == id) {
-      count++
-    }
-  })
-  return count
-}
+// function openCloseCart() {
+//   const containerCart = document.getElementsByClassName('cart-products')[0]
+//   containerCart.classList.forEach(item => { 
+//     if(item === 'hidden') {
+//       containerCart.classList.remove('hidden')
+//       containerCart.classList.add('active')
+//     }
+//     if (item === 'active') {
+//       containerCart.classList.remove('active')
+//       containerCart.classList.add('hidden')
+//     }
+//    })
+//   console.log(containerCart);
+// }
+
+// function addProduct(idProduct) {
+//   let arrayProductId = []
+//   let localStorageItems = localStorage.getItem(CARD_PRODUCTS)
+
+//   if(localStorageItems === null) {
+//     arrayProductId.push(idProduct)
+//     localStorage.setItem(CARD_PRODUCTS, arrayProductId)
+//   } else {
+//     let productsId = localStorage.getItem(CARD_PRODUCTS)
+//     if(productsId.length > 0) {
+//       productsId += ', ' + idProduct
+//     } else {
+//       productsId = productId
+//     }
+//     localStorage.setItem(CARD_PRODUCTS, productsId)
+//   }
+//   loadProductCard()
+// }
+
+// async function loadProductCard () {
+//   const products = await getProductsDb()
+//   //convetir resultado del localStorage en un array
+//   const localStorageItems = localStorage.getItem(CARD_PRODUCTS)
+//   let html = ''
+//   if (!localStorageItems) {
+//     html = `
+//       <div class="cart-product empty">
+//         <p>Carrito vacío</p>
+//       </div>
+//     `
+//   } else {
+//     const idProductsSplit = localStorageItems.split(', ')
+
+//     //eliminar los id duplicados
+//     const idProductCard = Array.from(new Set(idProductsSplit))
+
+//     idProductCard.forEach(id => {
+//       products.forEach(product => {
+//         if(id == product.id) {
+//           const quantity = countDuplicatesId(id, idProductsSplit)
+//           const totalPrice = product.price * quantity
+//           html += `
+//             <div class="cart-product">
+//               <img src="${product.image}" alt="${product.name}">
+//               <div class="cart-product-info">
+//                 <span class="quantity">${quantity}</span>
+//                 <p>${product.name}</p>
+//                 <p>${totalPrice.toFixed(2)}</p>
+//                 <div class="change-quantity">
+//                   <button>-</button>
+//                   <button>+</button>
+//                 </div>
+//                 <div class="cart-product-delete">
+//                   <button onclick="deleteProductCard(${product.id})">Eliminar</button>
+//                 </div>
+//               </div>
+//             </div>
+//           `
+//         }
+//       })
+//     })
+//   }
+//   document.getElementsByClassName('cart-products')[0].innerHTML = html
+// }
+
+// function deleteProductCard (idProduct){
+//   const idProductsCard = localStorage.getItem(CARD_PRODUCTS)
+//   const arrayProductsCard = idProductsCard.split(',')
+//   const resultIDDelete = deleteAllIds(idProduct, arrayProductsCard)
+
+//   if(resultDelete) {
+//     let count = 0
+//     let idsString = ''
+
+//     resultIDDelete.forEach(id => {
+//       count++
+//       if(count < resultDelete.length){
+        
+//       }
+//     })
+//   }
+// }
+
+// function countDuplicatesId(value, arrayIds) {
+//   let count = 0
+//   arrayIds.forEach(id => {
+//     if(value == id) {
+//       count++
+//     }
+//   })
+//   return count
+// }
+
+// function deleteAllIds (id, arrayIds) {
+//   return arrayIds.filter(itemId => {
+//     return itemId != id
+//   })
+// }
